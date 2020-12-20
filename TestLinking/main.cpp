393cad829a96937d4c1c06a36068fd803ba3197f
Stuart Mathews <stumathews@gmail.com>
2020-12-20 19:47:08 +0000

    add test linking project

---
+++ TestLinking/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+#include "audio/AudioManager.h"
+#include "common/global_config.h"
+#include "events/event_manager.h"
+#include "font/font_manager.h"
+#include "graphic/sdl_graphics_manager.h"
+#include "resource/resource_manager.h"
+#include "scene/scene_manager.h"
+#include "scene/Square.h"
+
+
+#pragma comment(lib, "cppgamelib")
+using namespace std;
+using namespace gamelib;
+
+int main(int argc, char **argv)
+{
+	const shared_ptr<gamelib::global_config> global_config(new gamelib::global_config);
+	const shared_ptr<gamelib::event_manager> event_admin(new gamelib::event_manager(global_config));
+		event_admin->initialize();
+	const shared_ptr<gamelib::font_manager> font_admin(new gamelib::font_manager);		
+	const shared_ptr<gamelib::audio_manager> audio_admin(new gamelib::audio_manager);
+	const shared_ptr<gamelib::sdl_graphics_manager> graphics_admin(new gamelib::sdl_graphics_manager(event_admin));
+		graphics_admin->initialize();
+	const shared_ptr<gamelib::resource_manager> resource_admin(new gamelib::resource_manager(global_config, graphics_admin, font_admin, audio_admin ));
+		resource_admin->initialize(event_admin);
+	const shared_ptr<gamelib::scene_manager> scene_admin(new gamelib::scene_manager(event_admin, global_config, resource_admin));
+		scene_admin->initialize();
+	
+	cout << "Hello World" << endl;
+	int i;
+	cin >> i;
+	return 0;
+}

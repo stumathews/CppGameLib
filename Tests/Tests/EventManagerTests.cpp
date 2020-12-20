dbc52c97ccb7dcdf15030a98b8c00661ab6ed87f
Stuart Mathews <stumathews@gmail.com>
2020-12-20 19:47:24 +0000

    tests for the library

---
+++ Tests/Tests/EventManagerTests.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+
+using namespace std;
+
+TEST(EventManager, NullTest)
+{
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
---
+++ Tests/Tests/SettingsManagerTests.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include <util/settings_manager.h>
+using namespace gamelib;
+using namespace std;
+
+TEST(SettingsManager, OpenFileTest)
+{
+	const auto expected_settings_count = 15;
+	settings_manager settings;
+	EXPECT_TRUE(settings.load("settings.xml"));
+
+	// Should have loaded 13 settings by default from the settings file
+	EXPECT_TRUE(settings.count() == expected_settings_count);
+}
+
+TEST(SettingsManager, AddSetting)
+{
+	const auto expected_settings_count = 18;
+	settings_manager settings;
+	settings.load();	
+	settings.add_setting("global", "name", setting_details("name", "mazer", "string"));
+	settings.add_setting("global", "genre", setting_details("name", "platformer", "string"));
+	settings.add_setting("global", "platform", setting_details("name", "windows", "string"));
+
+	// Should have added 3 extra settings
+	EXPECT_TRUE(settings.count() == expected_settings_count);
+}
+
+TEST(SettingsManager, GetSetting)
+{
+	settings_manager settings;
+	settings.load();	
+	settings.add_setting("global", "name", setting_details("name", "mazer", "string"));
+	settings.add_setting("global", "genre", setting_details("name", "platformer", "string"));
+
+	// Should be able to retrieve the settings we added
+	EXPECT_EQ(settings.get_string("global", "name"), "mazer");
+	EXPECT_EQ(settings.get_string("global", "genre"), "platformer");
+}
+
+TEST(SettingsManager, GetSettingType)
+{
+	settings_manager settings;
+	settings.load();
+	EXPECT_EQ(4, settings.get_int("global", "max_loops"));
+	EXPECT_EQ("Stuart", settings.get_string("global", "author"));
+	EXPECT_EQ(true, settings.get_bool("global", "verbose"));
+	EXPECT_EQ(123456789L, settings.get_long("global", "longnumber"));
+}

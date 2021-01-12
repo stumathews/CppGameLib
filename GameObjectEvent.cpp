acbf70d8788d72a546316801363d05630cd35e5f
Stuart Mathews <stumathews@gmail.com>
2021-01-12 21:09:49 +0000

    add generic game object event

---
+++ GameObjectEvent.cpp
@@ -0,0 +1,1 @@
+#include "GameObjectEvent.h"
---
+++ GameObjectEvent.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <memory>
+
+#include "events/Event.h"
+namespace gamelib
+{
+	class GameObject;
+
+	enum class GameObjectEventContext
+	{
+		Remove
+	};
+
+	class GameObjectEvent : public event
+	{
+	public:
+		GameObjectEvent(const int event_id, GameObject *gameObject, GameObjectEventContext context ) : event(event_type::GameObject, event_id), context(context), game_object(std::move(gameObject))
+		{
+		}
+
+		event_type get_type() override { return event_type::GameObject; }
+		std::string to_str() override { return "GameObjectEvent"; }
+		GameObjectEventContext context;
+		GameObject *game_object;
+	};
+}
+

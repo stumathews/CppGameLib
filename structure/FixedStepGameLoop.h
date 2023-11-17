3425ff2ccd089bfb48129b732bb16534e6cbbb36
Stuart Mathews <stumathews@gmail.com>
2023-11-17 17:15:31 +0000

    Hide some members that need not be public

--- structure/FixedStepGameLoop.h
+++ structure/FixedStepGameLoop.h
@@ -11,12 +11,12 @@ namespace gamelib
 		FixedStepGameLoop(unsigned int timeStepMs,
 			std::function<void(unsigned long deltaMs)> updateFunc, 
 			std::function<void()> drawFunc,
-			std::function<void(unsigned long deltaMs)> inputFunc);
-
+			std::function<void(unsigned long deltaMs)> inputFunc);		
+		void Loop(const GameWorldData* gameWorldData) override;
+	private:
 		static long GetTimeNowMs();
 		void Update(unsigned long deltaMs) override;
 		void Draw() override;
-		void Loop(const GameWorldData* gameWorldData) override;
 		std::function<void(unsigned long deltaMs)> UpdateFunc;
 		std::function<void()> DrawFunc;
 		std::function<void(unsigned long deltaMs)> InputFunc;

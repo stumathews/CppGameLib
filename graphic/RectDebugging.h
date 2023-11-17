0d910010386bc3781fb3318dbadc794762b9eb53
Stuart Mathews <stumathews@gmail.com>
2023-11-17 22:30:20 +0000

    add comments for readability's sake

--- graphic/RectDebugging.h
+++ graphic/RectDebugging.h
@@ -14,13 +14,18 @@ namespace gamelib
 	public:
 		static void PrintInRect(SDL_Renderer* renderer, const std::string& text, const SDL_Rect* bounds, const SDL_Color color)
 		{
-			SDL_Surface * surface = TTF_RenderText_Solid(std::static_pointer_cast<FontAsset>(
-				ResourceManager::Get()->GetAssetInfo("kenvector_future2.ttf"))->GetFont(), 
-				text.c_str(), 
-				color);
+			const auto font = std::static_pointer_cast<FontAsset>(ResourceManager::Get()->GetAssetInfo("kenvector_future2.ttf"))->GetFont();
 
-			SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, surface);	
+			// text -> surface
+			SDL_Surface * surface = TTF_RenderText_Blended(font, text.c_str(), color);
+			
+			// surface -> Texture
+			SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+			// texture -> renderer
 			SDL_RenderCopy(renderer, texture, nullptr, bounds);
+
+			// Cleanup
 			SDL_DestroyTexture(texture);
 			SDL_FreeSurface(surface);
 		}

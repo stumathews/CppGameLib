48445143612fb6092c36c9ad3309afe5e1e91fd8
Stuart Mathews <stumathews@gmail.com>
2024-05-07 12:14:29 +0200

    Add RingBuffer data structure

---
+++ Tests/Tests/RingBufferTests.cpp
@@ -0,0 +1,68 @@
+﻿
+#include <gtest/gtest.h>
+
+#include "utils/RingBuffer.h"
+
+using namespace gamelib;
+
+class RingBufferTests : public testing::Test
+{
+public:	
+
+	void SetUp() override
+	{
+	}
+
+	void TearDown() override
+	{
+	}
+};
+
+TEST_F(RingBufferTests, RingBufferBasic)
+{
+	constexpr auto data1 = 1;
+	constexpr auto data2 = 2;
+	constexpr auto data3 = 3;
+	constexpr auto data4 = 4;
+	constexpr auto bufferSize = 2;
+
+	RingBuffer<int> buffer(bufferSize);
+
+	// add data to buffer
+
+	buffer.Put(99, data1);
+	buffer.Put(100, data2);
+
+	const auto result1 = buffer.Get(99);
+	const auto result2 = buffer.Get(100);
+
+	// Overwrite buffer with new data
+	buffer.Put(101, data3);
+	buffer.Put(102, data4);
+	
+	const auto result3 = buffer.Get(101);
+	const auto result4 = buffer.Get(102);
+
+
+	// Try and use old position indexes
+	const auto result5 = buffer.Get(99);
+	const auto result6 = buffer.Get(100);
+	
+	// try get data for previously unset index
+
+	const auto result7 = buffer.Get(456);
+	
+	EXPECT_EQ(buffer.GetBufferSize(), bufferSize);
+
+	EXPECT_EQ(result1, data1);
+	EXPECT_EQ(result2, data2);
+	EXPECT_EQ(result3, data3);
+	EXPECT_EQ(result4, data4);
+
+	// should only correspond to the new data as that has overwritten the old data
+	EXPECT_EQ(result5, {});
+	EXPECT_EQ(result6, {});
+
+	// Unknown index should return {}
+	EXPECT_EQ(result7, {});
+}
---
+++ utils/RingBuffer.h
@@ -0,0 +1,60 @@
+﻿#pragma once
+#include <cstdint>
+
+namespace gamelib
+{
+	template <class T>
+	class RingBuffer
+	{
+	public:
+
+		explicit RingBuffer(int bufferSize = 1024)
+		{
+			this->bufferSize = bufferSize;
+
+			sequenceBuffer = new uint32_t[bufferSize];
+			sequenceBufferData = new T[bufferSize];
+			sequenceBufferData2 = std::vector<T>(bufferSize);
+
+		}		
+		
+		T Get(const uint16_t sequence)
+		{
+		    const int index = GetSequenceIndex(sequence);
+			if(sequenceBuffer[index] == sequence)
+			{
+				return sequenceBufferData2[index];
+			}
+		    return {};
+		}
+
+		void Put(const uint16_t sequence, T data)
+		{
+			const int index = GetSequenceIndex(sequence);
+			sequenceBuffer[index] = sequence;
+			sequenceBufferData2[index] = data;
+			lastAddedSequence = sequence;
+		}
+
+		[[nodiscard]] uint16_t GetLastAddedSequence() const { return lastAddedSequence; }
+
+		[[nodiscard]] uint16_t GetBufferSize() const { return bufferSize; }
+
+		[[nodiscard]] std::vector<T> GetDataBuffer() const { return sequenceBufferData2.data(); }
+
+		[[nodiscard]] uint16_t GetSequenceIndex(const uint16_t sequence) const
+		{
+			return sequence % bufferSize;
+		}
+		
+	private:
+
+		uint16_t sequence = 0;		
+		uint16_t lastAddedSequence = 0;		
+
+		int bufferSize{};
+		uint32_t* sequenceBuffer = {};
+		std::vector<T> sequenceBufferData2 = {};
+		T* sequenceBufferData = {}; // remove when ready
+	};
+}

44c74338c95e0f65a25dc6312eeeef545b01b385
Stuart Mathews <stumathews@gmail.com>
2024-05-07 12:14:41 +0200

    Add BitFiddler

---
+++ utils/BitFiddler.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include <cstdint>
+
+namespace gamelib
+{
+	template <class T>
+	class BitFiddler
+	{
+	public:
+		// Set bit n on number
+		static T SetBit(T number, T n)
+		{
+			return number | ((T)1 << n);
+		}
+
+		// clear bt n on number
+		static T ClearBit(T number, T n) {
+			return number & ~((T)1 << n);
+		}
+
+		// toggle/invert bit n on number
+		static T ToggleBit(T number, T n) {
+	    return number ^ ((T)1 << n);
+		}
+
+		// test bit n if set on number
+		static bool BitCheck(T number, T n)
+		{
+		    return (number >> n) & (T)1;
+		}
+
+		// set bit n or not
+		static T SetBitTo(T number, T n, bool x) {
+			return (number & ~((T)1 << n)) | ((T)x << n);
+		}		
+	};
+}

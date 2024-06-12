0c78e530b9e1b38273dbfa68416165b27ed5132c
Stuart Mathews <stumathews@gmail.com>
2024-06-12 18:38:55 +0100

    include crc routine and a test

---
+++ Tests/Tests/CrcTests.cpp
@@ -0,0 +1,34 @@
+﻿
+#include "pch.h"
+#include "net/crc.h"
+
+using namespace gamelib;
+
+class CrcTests : public testing::Test
+{
+public:	
+
+	void SetUp() override
+	{
+	}
+
+	void TearDown() override
+	{
+	}
+};
+
+
+
+TEST_F(CrcTests, CRC32)
+{
+	const auto buffer = "There can be only one.";
+	const auto crc = crc32buf(buffer, strlen(buffer));
+
+	const auto buffer1 = "Dude where is my car?";
+	const auto crc1 = crc32buf(buffer1, strlen(buffer1));
+
+	EXPECT_EQ(crc, 0x435ef19c);
+	EXPECT_NE(crc1, 0x435ef19c);
+	EXPECT_EQ(crc1, 0x767f565e);
+
+}

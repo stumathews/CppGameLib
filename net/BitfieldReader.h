4f15e8c18fd37b9363599247ccc84018aca538a2
Stuart Mathews <stumathews@gmail.com>
2024-07-06 23:21:29 +0100

    redesign bitfield reader

--- net/BitfieldReader.h
+++ net/BitfieldReader.h
@@ -14,6 +14,12 @@ namespace gamelib
 	{
 	public:
 		explicit BitfieldReader(T* field, const size_t elements): field(field), elements(elements) { }		
+				
+		void Finish()
+		{
+			// This call should be used at the same sequence as calls to packer's Finish method to cater for its effects while reading from the buffer
+			ReadNext<T>(bitsLeftInSegment);
+		}
 
 		/**
 		 * \brief Reads the next set of bits since the last call
@@ -25,23 +31,40 @@ namespace gamelib
 		OType ReadNext(const size_t numBits)
 		{
 			auto startBit = (bitsRead + numBits) - 1;
-
+			const auto maxStartBit = fieldSizeBits-1;
+			
 			if(numBits >= (fieldSizeBits * 2)) throw std::exception("Can't read more than the double the buffer size.");
 
 			if(countTimesOverflowed >= elements) throw std::exception("Buffer overflow.");
 			
-			if(startBit > fieldSizeBits-1)
+			if(startBit > maxStartBit)
 			{							
 				countTimesOverflowed++;
-
+				segmentsRead++;
 				return MakeMergedUnit<OType>(numBits, startBit);
 			}
-
 			auto bitsValue = BitFiddler<T>::GetBitsValue(*(field+countTimesOverflowed), startBit, numBits);
-			auto peek = BitFiddler<T>::ToString(bitsValue);
 			bitsRead += numBits;
+
+			totalBitsRead += numBits;
+			
+			bitsLeftInSegment = fieldSizeBits - bitsRead;
+
+			if(bitsRead % fieldSizeBits == 0)
+			{
+				countTimesOverflowed++;
+				segmentsRead++;
+				bitsLeftInSegment = fieldSizeBits;
+				bitsRead = 0;
+			}
 			return static_cast<OType>(bitsValue);
 		}
+
+		void FetchBytes(char* outBuffer, int outBufferSize)
+		{
+			memcpy_s(outBuffer, outBufferSize, field+countTimesOverflowed, outBufferSize);			
+		}
+
 
 		/**
 		 * \brief Returns an interval of bits from starting at high-bit position backwards for numBits bits
@@ -56,7 +79,8 @@ namespace gamelib
 			if(numBits >= (fieldSizeBits * 2)) throw std::exception("Can't read more than the double the buffer size.");
 
 			if(startHighBit > fieldSizeBits)
-			{						
+			{
+				// Can't read all of the value from within the current segment, 
 				return MakeMergedUnit<OType>(numBits, startHighBit);
 			}
 
@@ -66,12 +90,18 @@ namespace gamelib
 		/**
 		 * \brief Restarts the reader's position at bit 0
 		 */
-		void Reset() { bitsRead = 0; }
+		void Reset() { bitsRead = bitsLeftInSegment = countTimesOverflowed = 0; }
+
+		uint8_t BitsLeftInSegment() const { return bitsLeftInSegment; }
+		uint8_t SegmentsRead() const { return segmentsRead; }
+		T TotalBitsRead() { return totalBitsRead;} 
 
 	private:
 		template <typename OType>
 		OType MakeMergedUnit(const size_t numBits, unsigned long long startBit)
 		{
+			// Can't read all of the value from within the current segment:
+
 			// LHS [1]  RHS [0]
 			// 00000001 001_11000
 			
@@ -80,19 +110,19 @@ namespace gamelib
 
 			// we are going construct a new T from bits from the current block and some bits that overflowed and that exist in the next block
 			std::remove_const_t<T> merged = (merged & 0); // note we remove const if T is const to make a non-const T that we can modify
-
-			auto rhsLastIndex = (fieldSizeBits-1);
+			
+			auto maxStartBit = (fieldSizeBits-1);
 			// work out how many bits come from the left hand side (lhs) and how many from right hand side (rhs)				
 
 			auto numBitsLhs = startBit - (fieldSizeBits-1);
 			auto numBitsRhs = numBits - numBitsLhs;
 
-				// get the bits from the rhs buffer and put them into the new T
-				merged = BitFiddler<T>::GetBitsValue(*(field+countTimesOverflowed-1), rhsLastIndex, numBitsRhs);
-			
+			// get the bits from the rhs buffer and put them into the new T
+				merged = BitFiddler<T>::GetBitsValue(*(field+countTimesOverflowed-1), maxStartBit, numBitsRhs);
+
 			// put the bits from the next buffer after them in T, so T looks like this |lhsbits|rhsbits|
 			merged = BitFiddler<T>::SetBits(merged, (numBitsRhs+numBitsLhs)-1, numBitsLhs, *(field+countTimesOverflowed));
-					
+
 			// If this is exactly a multiple of base type for the buffer, eg bit 61 down to 31 (and base of buffer is uint32_t)
 			// no need to shift as there are 0 bits in the rhs and all thebits in the lhs
 			const auto isMultipleOfT = (startBit-1) % (fieldSizeBits-1) == 0;
@@ -106,11 +136,17 @@ namespace gamelib
 			// say that we must start reading from the next block (lhs) after the bits we've already used from it
 			// this is so that the next read, does not re-read the bits we've already borrowed from it to form the new merged T block
 			bitsRead = numBitsLhs;
-				
+			
+			bitsLeftInSegment = fieldSizeBits - bitsRead;
+
+			totalBitsRead += numBits;
+							
 			// return the merged block, T
 			return merged;
 		}
 		uint8_t bitsRead {0};
+		uint8_t segmentsRead {0};
+		uint8_t bitsLeftInSegment {0};
 		uint8_t totalBitsRead {0};
 		T* field;
 		uint8_t fieldSizeBits {sizeof(T)*8};

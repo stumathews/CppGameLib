8a9e34aee1e0f6f00139762c9770315be65f23f2
Stuart Mathews <stumathews@gmail.com>
2024-07-13 14:09:18 +0100

    Read/Write a packet Datum

--- net/PacketDatum.cpp
+++ net/PacketDatum.cpp
@@ -5,17 +5,7 @@ gamelib::PacketDatum::PacketDatum()
 	Acked = false;
 }
 
-gamelib::PacketDatum::PacketDatum(const bool acked, const char* customData): Acked(acked), data(customData)
+gamelib::PacketDatum::PacketDatum(const bool acked, const char* customData): Acked(acked), dataString(customData)
 {
 	
-}
-
-void gamelib::PacketDatum::Write(gamelib::BitPacker<uint32_t>& bitPacker) const
-{
-	bitPacker.Pack(BITS_REQUIRED( 0, 1), Acked);
-}
-
-void gamelib::PacketDatum::Read(BitfieldReader<const char>& bitfieldReader)
-{
-	Acked = bitfieldReader.ReadNext<uint16_t>(16);
 }
--- net/PacketDatum.h
+++ net/PacketDatum.h
@@ -4,26 +4,40 @@
 
 #include <cstdint>
 
+#include "BitPackingTypes.h"
 #include "net/BitfieldReader.h"
 #include "net/BitPacker.h"
 
 namespace gamelib
 {
 	struct PacketDatum
-	{
+	{	
 		PacketDatum();
 		explicit PacketDatum(bool acked, const char* customData = "");
 
-		void Write(BitPacker<uint32_t>& bitPacker) const;
+		template <typename T>		
+		void Write(BitPacker<T>& bitPacker) const
+		{
+			bitPacker.Pack(BITS_REQUIRED( 0, 1), Acked);
+			bitPacker.Pack(16, Sequence);
+			bit_packing_types::String<T>(dataString).Write(bitPacker);
+		}
 
-		void Read(BitfieldReader<const char>& bitfieldReader);
+		template <typename T>
+		void Read(BitfieldReader<T>& bitfieldReader)
+		{
+			Acked = bitfieldReader.ReadNext<uint16_t>(BITS_REQUIRED(0, 1));
+			Sequence = bitfieldReader.ReadNext<uint16_t>(16);
+			bit_packing_types::String<T> temp;
+			temp.Read(bitfieldReader);
+			dataString = temp.c_str();
+		}
 
 		bool Acked;
 		uint16_t Sequence {};
-		double SendTime {};
-		const char* data{};
-
-
+		[[nodiscard]] const char* Data() const { return dataString.c_str();}
+	private:
+		std::string dataString;
 	};
 }
 #endif

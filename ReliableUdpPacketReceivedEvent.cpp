19691fa5a0f31fe92d4f6f7f59015e85238e6ab6
Stuart Mathews <stumathews@gmail.com>
2024-07-20 01:28:36 +0100

    Add new ReliableUdpPacketReceivedEvent event

---
+++ ReliableUdpPacketReceivedEvent.cpp
@@ -0,0 +1,1 @@
+#include "ReliableUdpPacketReceivedEvent.h"
---
+++ ReliableUdpPacketReceivedEvent.h
@@ -0,0 +1,24 @@
+#ifndef RELIABLE_UDP_PACKET_RECEIVED_EVENT_H
+#define RELIABLE_UDP_PACKET_RECEIVED_EVENT_H
+#pragma once
+#include "events/Event.h"
+#include "events/EventNumbers.h"
+#include "net/Message.h"
+
+namespace gamelib
+{
+	const static EventId ReliableUdpPacketReceivedEventId(ReliableUdpPacketReceived, "ReliableUdpPacketReceived");
+	class ReliableUdpPacketReceivedEvent final : public Event
+	{
+	public:
+
+		explicit ReliableUdpPacketReceivedEvent(const std::shared_ptr<Message> message) : Event(ReliableUdpPacketReceivedEventId), ReceivedMessage(message)
+		{
+		}
+
+		const std::shared_ptr<Message> ReceivedMessage;
+	};
+
+}
+
+#endif RELIABLE_UDP_PACKET_RECEIVED_EVENT_H

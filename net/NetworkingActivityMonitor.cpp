3ae400ed1d89929f9dc1b2ab44059c42156dae71
Stuart Mathews <stumathews@gmail.com>
2025-04-18 23:36:58 +0100

    Create a NetworkingActivityMonitor

---
+++ net/NetworkingActivityMonitor.cpp
@@ -0,0 +1,258 @@
+
+#include "NetworkingActivityMonitor.h"
+#include <processes/ProcessManager.h>
+#include <events\event.h>
+#include <events/NetworkPlayerJoinedEvent.h>
+
+using namespace std;
+
+namespace gamelib
+{
+	NetworkingActivityMonitor::NetworkingActivityMonitor(ProcessManager& processManager, EventManager& eventManager):
+		processManager(processManager), eventManager(eventManager)
+	{
+		// Prepare a file that will receive the statistics every saveIntervalMs
+		statisticsFile = std::make_shared<TextFile>("statistics.txt");
+	}
+
+	void NetworkingActivityMonitor::Initialise()
+	{
+		constexpr auto saveIntervalMs = 1000; // every second
+
+		// Subscribe to Network Events
+		eventManager.SubscribeToEvent(NetworkTrafficReceivedEventId, this);
+		eventManager.SubscribeToEvent(ReliableUdpPacketReceivedEventId, this);
+		eventManager.SubscribeToEvent(ReliableUdpCheckSumFailedEventId, this);
+		eventManager.SubscribeToEvent(ReliableUdpPacketLossDetectedEventId, this);
+		eventManager.SubscribeToEvent(ReliableUdpAckPacketEventId, this);
+		eventManager.SubscribeToEvent(ReliableUdpPacketRttCalculatedEventId, this);
+
+		// Schedule save operation to run periodically
+		statisticsSampleInterval.SetFrequency(saveIntervalMs);
+
+		InitialiseStatisticsFile();
+
+		// Schedule sampler to run as a background process
+		ScheduleSaveStatistics();
+	}
+
+	void NetworkingActivityMonitor::InitialiseStatisticsFile() const
+	{
+		// Setup statistics file header (first line)
+		std::stringstream header;
+		header << "TimeSecs" << "\t"
+			<< "BytesReceived" << "\t"
+			<< "CountPacketsLost" << "\t"
+			<< "CountPacketsReceived" << "\t"
+			<< "AverageLatencyMsSma3" << "\t"
+			<< "CountAcks" << "\t"
+			<< "VerificationFailedCount" << "\t"
+			<< "CountAggregateMessagesReceived" << "\t"
+			<< "SendingRateMs" << "\t"
+			<< "SendingRatePs" << "\t"
+			<< "RttMs"
+			<< "\n";
+
+		statisticsFile->Append(header.str(), false);
+	}
+
+	void NetworkingActivityMonitor::AppendStatsToFile(const int tSeconds) const
+	{
+		std::stringstream message;
+
+		// Fetch statistics and add them to the log file (see header)
+		message
+			<< tSeconds << "\t"
+			<< stats.BytesReceived << "\t"
+			<< stats.CountPacketsLost << "\t"
+			<< stats.CountPacketsReceived << "\t"
+			<< stats.AverageLatencySMA3 << "\t"
+			<< stats.CountAcks << "\t"
+			<< stats.VerificationFailedCount << "\t"
+			<< stats.CountAggregateMessagesReceived << "\t"
+			<< stats.SendRateMs << "\t"
+			<< stats.SendRatePs << "\t"
+			<< stats.RttMs
+			<< "\n";
+
+		// Write to file
+		statisticsFile->Append(message.str(), false);
+	}
+
+	void NetworkingActivityMonitor::SaveAndReset(const unsigned long deltaMs)
+	{
+		// Progress the interval
+		statisticsSampleInterval.Update(deltaMs);
+
+		// Will only write statistics to file if the statistics sampling interval has elapsed (ready)
+		statisticsSampleInterval.DoIfReady([&]()
+		{
+			// Record the current second we are in since game was started, this will be the independent variable
+			const auto tNowSecs = static_cast<int>(GameDataManager::Get()->GameWorldData.ElapsedGameTime) / 1000;
+
+			AppendStatsToFile(tNowSecs);
+
+			// Reset statistics to zero.
+			stats.Reset();
+		});
+	}
+
+	void NetworkingActivityMonitor::ScheduleSaveStatistics()
+	{
+		// Schedule process to write statistics to file
+
+		const auto process = std::make_shared<Action>([this](auto&& deltaMs) { SaveAndReset(deltaMs); }, false);
+
+		ScheduleProcess(process);
+	}
+
+	// ReSharper disable once CppPassValueParameterByConstReference
+	void NetworkingActivityMonitor::ScheduleProcess(shared_ptr<Process> process) const
+	{
+		// Add this process to the process manager - schedules process to receive updates
+		processManager.AttachProcess(std::static_pointer_cast<Process>(process));
+	}
+
+	ListOfEvents NetworkingActivityMonitor::HandleEvent(const std::shared_ptr<Event>& evt, const unsigned long inDeltaMs)
+	{
+		if (evt->Id.PrimaryId == NetworkPlayerJoinedEventId.PrimaryId) { OnNetworkPlayerJoinedEvent(evt); }
+		if (evt->Id.PrimaryId == ReliableUdpPacketReceivedEventId.PrimaryId) { OnReliableUdpPacketReceivedEvent(evt); }
+		if (evt->Id.PrimaryId == ReliableUdpCheckSumFailedEventId.PrimaryId) { OnReliableUdpCheckSumFailedEvent(evt); }
+		if (evt->Id.PrimaryId == ReliableUdpPacketLossDetectedEventId.PrimaryId) { OnReliableUdpPacketLossDetectedEvent(evt); }
+		if (evt->Id.PrimaryId == ReliableUdpAckPacketEventId.PrimaryId) { OnReliableUdpAckPacketEvent(evt); }
+		if (evt->Id.PrimaryId == ReliableUdpPacketRttCalculatedEventId.PrimaryId) { OnReliableUdpPacketRttCalculatedEvent(evt); }
+		if (evt->Id.PrimaryId == NetworkTrafficReceivedEventId.PrimaryId) { OnNetworkTrafficReceivedEvent(evt); }
+		return {};
+	}
+
+	std::string NetworkingActivityMonitor::GetSubscriberName()
+	{
+		return "NetworkStatistics";
+	}
+	
+	void NetworkingActivityMonitor::OnNetworkPlayerJoinedEvent(const std::shared_ptr<Event>& evt)
+	{
+		const auto joinEvent = To<NetworkPlayerJoinedEvent>(evt);
+		stringstream message;
+		message << joinEvent->Player.GetNickName() << " joined.";
+		Logger::Get()->LogThis(message.str());
+	}
+
+	void NetworkingActivityMonitor::OnNetworkTrafficReceivedEvent(const std::shared_ptr<Event>& evt)
+	{
+		const auto networkPlayerTrafficReceivedEvent = To<NetworkTrafficReceivedEvent>(evt);
+		std::stringstream message;
+		message
+			<< networkPlayerTrafficReceivedEvent->BytesReceived << " bytes of data received from " << networkPlayerTrafficReceivedEvent->Identifier
+			<< ". Message: \"" << networkPlayerTrafficReceivedEvent->Message << "\"";
+
+		// how much data did we receie in this second?
+		stats.BytesReceived += networkPlayerTrafficReceivedEvent->BytesReceived;
+
+		Logger::Get()->LogThis(message.str());
+	}
+
+	void NetworkingActivityMonitor::OnReliableUdpPacketReceivedEvent(const std::shared_ptr<Event>& evt)
+	{
+		const auto rudpEvent = To<ReliableUdpPacketReceivedEvent>(evt);
+		const auto rudpMessage = rudpEvent->ReceivedMessage;
+		stringstream bundledSeqs;
+
+		bundledSeqs << "(";
+		for (int i = 0; i < rudpMessage->DataCount(); i++)
+		{
+			bundledSeqs << rudpMessage->Data()[i].Sequence;
+			if (i < rudpMessage->DataCount() - 1)
+			{
+				bundledSeqs << ",";
+			}
+		}
+		bundledSeqs << ")";
+
+		std::stringstream message;
+		message
+			<< "Received " << rudpMessage->Header.Sequence << ". Playload: " << bundledSeqs.str()
+			<< " Sender acks: "
+			<< BitFiddler<uint32_t>::ToString(rudpMessage->Header.LastAckedSequence);
+
+		// write stats
+		stats.CountPacketsReceived++;
+		stats.CountAggregateMessagesReceived += rudpMessage->DataCount();
+
+		Logger::Get()->LogThis(message.str());
+	}
+
+	void NetworkingActivityMonitor::OnReliableUdpCheckSumFailedEvent(const std::shared_ptr<Event>& evt)
+	{
+		const auto failedChecksumEvent = To<ReliableUdpCheckSumFailedEvent>(evt);
+		const auto failedMessage = failedChecksumEvent->failedMessage;
+		std::stringstream message;
+
+		message << "Checksum failed for sequence " << failedMessage->Header.Sequence << ". Dropping packet.";
+
+		// write stats
+		stats.VerificationFailedCount++;
+
+		Logger::Get()->LogThis(message.str());
+	}
+
+	void NetworkingActivityMonitor::OnReliableUdpPacketLossDetectedEvent(const std::shared_ptr<Event>& evt)
+	{
+		const auto reliableUdpPacketLossDetectedEvent = To<ReliableUdpPacketLossDetectedEvent>(evt);
+		const auto resendingMessage = reliableUdpPacketLossDetectedEvent->messageBundle;
+
+		stringstream bundledSeqs;
+		std::stringstream message;
+
+		for (int i = static_cast<int>(resendingMessage->Data().size()) - 1; i >= 0; i--)
+		{
+			bundledSeqs << resendingMessage->Data()[i].Sequence;
+			if (i < resendingMessage->DataCount() - 1)
+			{
+				bundledSeqs << ",";
+			}
+		}
+
+		message << "Packet loss detected. Sequences " << bundledSeqs.str() << " were not acknowledged by receiver and will be resent with sending sequence "
+			<< resendingMessage->Header.Sequence;
+
+		// Track statistics
+		stats.CountPacketsLost++;
+
+		Logger::Get()->LogThis(message.str());
+	}
+
+	void NetworkingActivityMonitor::OnReliableUdpAckPacketEvent(const std::shared_ptr<Event>& evt)
+	{
+		const auto reliableUdpPacketLossDetectedEvent = To<ReliableUdpAckPacketEvent>(evt);
+		const auto ackMessage = reliableUdpPacketLossDetectedEvent->ReceivedMessage;
+
+		std::stringstream message;
+
+		message << "Acknowledgement " << (reliableUdpPacketLossDetectedEvent->Sent ? "sent: " : " received: ") << ackMessage->Header.Sequence;
+
+		// how many acks did we receive
+		stats.CountAcks++;
+
+		Logger::Get()->LogThis(message.str());
+
+	}
+
+	void NetworkingActivityMonitor::OnReliableUdpPacketRttCalculatedEvent(const std::shared_ptr<gamelib::Event>& evt)
+	{
+		const auto rttEvent = To<ReliableUdpPacketRttCalculatedEvent>(evt);
+
+		// The last latency recorded is a smooth moving average considering last 3 packets
+		stats.AverageLatencySMA3 = rttEvent->Rtt.Sma3;
+		stats.RttMs = static_cast<int>(rttEvent->Rtt.Rtt);
+	}
+
+	void NetworkingActivityMonitor::SetSendRateMs(const int ms)
+	{
+		stats.SendRateMs = ms;
+	}
+	void NetworkingActivityMonitor::SetSendRatePs(const int sec)
+	{
+		stats.SendRatePs = sec;
+	}
+}
---
+++ net/NetworkingActivityMonitor.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+#ifndef NETWORKING_ACTIVITY_MONITOR_H
+#define NETWORKING_ACTIVITY_MONITOR_H
+
+#include <GameDataManager.h>
+#include <processes\Action.h>
+#include <processes\Process.h>
+#include <utils\Utils.h>
+#include <file/TextFile.h>
+#include "NetworkingStatistics.h"
+
+namespace gamelib
+{
+	class NetworkingActivityMonitor : public gamelib::EventSubscriber
+	{
+	public:
+	
+		NetworkingActivityMonitor(ProcessManager& processManager, EventManager& eventManager);
+		void ScheduleSaveStatistics();
+		void ScheduleProcess(std::shared_ptr<Process> process) const;
+		void InitialiseStatisticsFile() const;
+		void AppendStatsToFile(int tSeconds) const;
+		void SaveAndReset(unsigned long deltaMs);
+		void Initialise();
+		ListOfEvents HandleEvent(const std::shared_ptr<Event>& evt, const unsigned long inDeltaMs) override;
+		std::string GetSubscriberName() override;
+		void SetSendRateMs(int ms);
+		void SetSendRatePs(int sec);
+				
+	private:
+
+		NetworkingStatistics stats;
+		std::shared_ptr<gamelib::TextFile> statisticsFile;
+		gamelib::PeriodicTimer statisticsSampleInterval;
+		ProcessManager& processManager;
+		EventManager& eventManager;
+
+		void OnReliableUdpAckPacketEvent(const std::shared_ptr<Event>& evt);
+		void OnReliableUdpPacketRttCalculatedEvent(const std::shared_ptr<gamelib::Event>& evt);
+		void OnNetworkTrafficReceivedEvent(const std::shared_ptr<Event>& evt);
+		void OnReliableUdpPacketReceivedEvent(const std::shared_ptr<Event>& evt);
+		void OnReliableUdpCheckSumFailedEvent(const std::shared_ptr<Event>& evt);
+		void OnReliableUdpPacketLossDetectedEvent(const std::shared_ptr<Event>& evt);
+		static void OnNetworkPlayerJoinedEvent(const std::shared_ptr<Event>& evt);
+	};
+}
+
+#endif

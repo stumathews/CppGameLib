bc1449c96b3b118f0b737fd278b0eca4ce6d11dc
Stuart Mathews <stumathews@gmail.com>
2025-06-26 21:10:06 +0200

    Refactoring

--- cppgamelib/ai.h
+++ cppgamelib/ai.h
@@ -5,10 +5,10 @@
 #include <ai/BehaviorResult.h>
 #include <ai/BehaviorTree.h>
 #include <ai/BehaviorTreeBuilder.h>
-#include <ai/Composite.h>
+#include <ai/CompositeBehavior.h>
 #include <ai/ConditionalSequence.h>
 #include <ai/Decorator.h>
-#include <ai/ContinueLastSuccessfulSequenceSelector.h>
+#include <ai/ActiveSelector.h>
 #include <ai/FSM.h>
 #include <ai/FSMState.h>
 #include <ai/FSMTransition.h>

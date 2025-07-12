a83624cbb742df5a8f141d6384bab9f79377de6e
Stuart Mathews <stumathews@gmail.com>
2025-07-12 11:58:31 +0200

    add MAP_IMPORTED_CONFIG_RELWITHDEBINFO and MAP_IMPORTED_CONFIG_MINSIZEREL to project config file

--- cmake/cppgamelibConfig.cmake
+++ cmake/cppgamelibConfig.cmake
@@ -1,1 +1,6 @@
 include("${CMAKE_CURRENT_LIST_DIR}/cppgamelib.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/cppgamelib-release.cmake")
+
+set_target_properties(cppgamelib::cppgamelib PROPERTIES
+        MAP_IMPORTED_CONFIG_RELWITHDEBINFO "RELEASE"
+        MAP_IMPORTED_CONFIG_MINSIZEREL "RELEASE" )

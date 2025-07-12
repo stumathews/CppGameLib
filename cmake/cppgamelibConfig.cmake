d20e2ea1643e1c09a37230650924fa3120b86c4f
Stuart Mathews <stumathews@gmail.com>
2025-07-12 12:11:19 +0200

    remove adding -release.cmake as its auto gen'd based on config

--- cmake/cppgamelibConfig.cmake
+++ cmake/cppgamelibConfig.cmake
@@ -1,5 +1,4 @@
 include("${CMAKE_CURRENT_LIST_DIR}/cppgamelib.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/cppgamelib-release.cmake")
 
 set_target_properties(cppgamelib::cppgamelib PROPERTIES
         MAP_IMPORTED_CONFIG_RELWITHDEBINFO "RELEASE"

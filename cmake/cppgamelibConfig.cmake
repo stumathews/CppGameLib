f59291c044cde4d09c9801547f9e7ce892ea145f
Stuart Mathews <stumathews@gmail.com>
2025-08-16 09:55:06 +0100

    remove references to MAP_IMPORTED_CONFIG_RELWITHDEBINFO

--- cmake/cppgamelibConfig.cmake
+++ cmake/cppgamelibConfig.cmake
@@ -1,5 +1,1 @@
 include("${CMAKE_CURRENT_LIST_DIR}/cppgamelib.cmake")
-
-set_target_properties(cppgamelib::cppgamelib PROPERTIES
-        MAP_IMPORTED_CONFIG_RELWITHDEBINFO "RELEASE"
-        MAP_IMPORTED_CONFIG_MINSIZEREL "RELEASE" )

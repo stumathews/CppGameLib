6d8531d8bf725eeb16d557c4415c56a84e362fa5
Stuart Mathews <stumathews@gmail.com>
2026-01-04 01:08:50 +0000

    Move scripted behavior into GameLib namespace

--- ai/ScriptedBehavior.h
+++ ai/ScriptedBehavior.h
@@ -23,75 +23,74 @@ extern "C" {
 namespace gamelib
 {
     class ScriptAsset;
-}
-
-class ScriptedBehavior : gamelib::Behavior
-{
-public:
-    explicit ScriptedBehavior(std::string resourceName, unsigned long runEveryMs = 0) : scriptName(std::move(resourceName))
+    class ScriptedBehavior : gamelib::Behavior
     {
-        // We will run our logic at a periodic frequency
-        logicTimer.SetFrequency(static_cast<int>(runEveryMs));
-    }
-
-    void OnInitialize() override
-    {
-        // Fetch script asset from resource manager
-        const auto asset = gamelib::ResourceManager::Get()->GetAssetInfo(scriptName);
-
-        LoadAssetIfNotLoaded(asset);
-
-        // Save it to a ScriptAsset
-        scriptAsset = gamelib::ScriptManager::ToScriptAsset(asset);
-
-        // Create a new Lua state for this script
-        luaState = luaL_newstate();
-
-        // Load Lua standard libraries
-        luaL_openlibs(luaState);
-    }
-
-    void OnTerminate() override
-    {
-        // Close lua state
-        lua_close(luaState);
-
-        // Note: No need to unload asset it gets unloaded automatically
-    }
-
-    gamelib::Status Update(unsigned long deltaMs) override
-    {
-        logicTimer.Update(deltaMs);
-
-        // We won't run this script on every frame, but will instead run it periodically to save resources
-        logicTimer.DoIfReady([&]()
+    public:
+        explicit ScriptedBehavior(std::string resourceName, unsigned long runEveryMs = 0) : scriptName(std::move(resourceName))
         {
-            // Execute a Lua script
-            luaL_dostring(luaState, scriptAsset->GetScriptContent()->c_str());
-        });
-
-        return gamelib::Status::Success;
-    }
-private:
-    std::shared_ptr<gamelib::ScriptAsset> scriptAsset;
-    lua_State* luaState = nullptr;
-    std::string scriptName;
-    gamelib::PeriodicTimer logicTimer;
-    void LoadAssetIfNotLoaded(const std::shared_ptr<gamelib::Asset> &asset) const
-    {
-        if (!asset->IsLoadedInMemory)
-        {
-            // Normally this is handled automatically by the scene manager
-            std::stringstream message;
-
-            message << "Resource '" << scriptName << "' was not pre-loaded. Did you forget to use the SceneManager to load assets by scene/level?";
-            gamelib::Logger::Get()->LogThis(message.str());
-
-            // Manually load the asset
-            asset->Load();
+            // We will run our logic at a periodic frequency
+            logicTimer.SetFrequency(static_cast<int>(runEveryMs));
         }
-    }
 
-};
+        void OnInitialize() override
+        {
+            // Fetch script asset from resource manager
+            const auto asset = gamelib::ResourceManager::Get()->GetAssetInfo(scriptName);
+
+            LoadAssetIfNotLoaded(asset);
+
+            // Save it to a ScriptAsset
+            scriptAsset = gamelib::ScriptManager::ToScriptAsset(asset);
+
+            // Create a new Lua state for this script
+            luaState = luaL_newstate();
+
+            // Load Lua standard libraries
+            luaL_openlibs(luaState);
+        }
+
+        void OnTerminate() override
+        {
+            // Close lua state
+            lua_close(luaState);
+
+            // Note: No need to unload asset it gets unloaded automatically
+        }
+
+        gamelib::Status Update(unsigned long deltaMs) override
+        {
+            logicTimer.Update(deltaMs);
+
+            // We won't run this script on every frame, but will instead run it periodically to save resources
+            logicTimer.DoIfReady([&]()
+            {
+                // Execute a Lua script
+                luaL_dostring(luaState, scriptAsset->GetScriptContent()->c_str());
+            });
+
+            return gamelib::Status::Success;
+        }
+    private:
+        std::shared_ptr<gamelib::ScriptAsset> scriptAsset;
+        lua_State* luaState = nullptr;
+        std::string scriptName;
+        gamelib::PeriodicTimer logicTimer;
+        void LoadAssetIfNotLoaded(const std::shared_ptr<gamelib::Asset> &asset) const
+        {
+            if (!asset->IsLoadedInMemory)
+            {
+                // Normally this is handled automatically by the scene manager
+                std::stringstream message;
+
+                message << "Resource '" << scriptName << "' was not pre-loaded. Did you forget to use the SceneManager to load assets by scene/level?";
+                gamelib::Logger::Get()->LogThis(message.str());
+
+                // Manually load the asset
+                asset->Load();
+            }
+        }
+
+    };
+}
 
 #endif //CPPGAMELIB_SCRIPTEDBEHAVIOR_H
